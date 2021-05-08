47075947d79b2a2ab9195d939105acac23bf8c91
Rafael Albuquerque <rafael.albuquerque.it@gmail.com>
2021-05-08 17:55:36 -0300

    Routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,36 +1,7 @@
-import DataTable from "components/DataTable";
-import Footer from "components/Footer";
-import NavBar from "components/NavBar";
-import BarChart from "./components/BarChart/index";
-import DonutChart from "./components/DonutChart/index";
+import Routes from "./Routes";
 
 function App() {
-  return (
-    <>
-      <NavBar />
-      <div className="container">
-        <h1 className="text-primary py-3">Dashboard de Vendas</h1>
-
-        <div className="row px-3">
-          <div className="col-sm-6">
-            <h5 className="text-center text-secondary">Taxa de sucesso (%)</h5>
-            <BarChart />
-          </div>
-          <div className="col-sm-6">
-            <h5 className="text-center text-secondary">Todas as vendas</h5>
-            <DonutChart />
-          </div>
-        </div>
-
-        <div className="py-3">
-          <h2 className="text-primary">Todas Vendas</h2>
-        </div>
-
-        <DataTable />
-      </div>
-      <Footer />
-    </>
-  );
+  return <Routes />;
 }
 
 export default App;
