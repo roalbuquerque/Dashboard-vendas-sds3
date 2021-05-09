6bca5e3c0c2447b507bbb4e7bed39daba85db918
Rafael Albuquerque <rafael.albuquerque.it@gmail.com>
2021-05-09 09:56:36 -0300

    BarChart integration

--- frontend/src/components/DonutChart/index.tsx
+++ frontend/src/components/DonutChart/index.tsx
@@ -10,6 +10,7 @@ type ChartData = {
 };
 
 const DonutChart = () => {
+  //useState
   const [chartData, setChartData] = useState<ChartData>({
     labels: [],
     series: [],
@@ -24,11 +25,6 @@ const DonutChart = () => {
       setChartData({ labels: myLabels, series: mySeries });
     });
   }, []);
-
-  // const mockData = {
-  //   series: [477138, 499928, 444867, 220426, 473088],
-  //   labels: ["Anakin", "Barry Allen", "Kal-El", "Logan", "Padmé"],
-  // };
 
   const options = {
     legend: {
